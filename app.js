407032a5d689dae446bb74accdf69eeb7d44be88
aqsaasif-20 <211.aqsaasif@gmail.com>
2025-04-27 16:34:18 +0500

    demo

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 // Initialize Gemini API client with your API key
-const genAI = new GoogleGenerativeAI("AIzaSyBpzD-l5kQ7LxeMyyixWZTs39Jgx9nzPnQ");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 app.post("/getResponse", async (req, res) => {
     try {
